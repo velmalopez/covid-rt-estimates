e160780d59ce678b81361c812605d59e5cac2661
Sam Abbott <s.e.abbott12@gmail.com>
2020-07-30 21:53:16 +0100

    Update update-via-docker.sh

--- bin/update-via-docker.sh
+++ bin/update-via-docker.sh
@@ -8,6 +8,7 @@ mkdir results
 docker run --rm --user rstudio --mount type=bind,source=$(pwd)/results,target=/home/covidrtestimates covidrtestimates /bin/bash bin/update-estimates.sh
 
 ## Move newly produced results and clean up
-mv -f results/cases cases
-mv -f results/deaths deaths
+mv -f results/national national
+mv -f results/subnational subnational
+mv -f results/last-update
 rm -r -f results
