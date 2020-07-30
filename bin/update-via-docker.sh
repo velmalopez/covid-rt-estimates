90aa16467bfab280ccb6e772c4a996396339247a
Sam Abbott <s.e.abbott12@gmail.com>
2020-07-30 22:23:57 +0100

    Update update-via-docker.sh

--- bin/update-via-docker.sh
+++ bin/update-via-docker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-## Make a results directory
-mkdir results
-
 ## Update estimates in newly built docker container
 ## This will use all cores available to docker by default
 sudo docker run --rm --user rstudio --mount type=bind,source=$(pwd),target=/home/rstudio/covid-rt-estimates --name covidrtestimates covidrtestimates /bin/bash bin/update-estimates.sh
