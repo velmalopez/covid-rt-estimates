31262c5d9b03b8abade3d5d01a9cb74fda090af7
seabbs <s.e.abbott12@gmail.com>
2020-07-31 10:55:33 +0000

    add global truncation of data by 3 days

--- R/utils.R
+++ R/utils.R
@@ -21,21 +21,22 @@ check_for_update <- function(cases, last_run) {
   current_max_date <- max(cases$date, na.rm = TRUE)
     
   if (file.exists(last_run)){
-    last_run <- readRDS(last_run)
+    last_run_date <- readRDS(last_run)
    
-    if (current_max_date >= last_run) {
+    if (current_max_date <= last_run_date) {
       stop("Data has not been updated since last run. 
-      If wanting to run again then remove ", last_run)
-    }else{
-      saveRDS(current_max_date, last_run)
-     }
+      If wanting to run again then remove ", last_run_date)
+    }
   }
+    
+  saveRDS(current_max_date, last_run)
   
   return(invisible(NULL))
 }
 #' Clean regional data
 clean_regional_data <- function(cases) {
   cases <- cases[, .(region, date = as.Date(date), confirm = cases_new)]
+  cases <- cases[, .SD[date <= (max(date, na.rm = TRUE) - lubridate::days(3))], by = region]
   cases <- cases[, .SD[date >= (max(date) - lubridate::weeks(8))], by = region]
   cases <- cases[!is.na(confirm)]
   data.table::setorder(cases, date)
@@ -48,7 +49,7 @@ regional_epinow_with_settings <- function(reported_cases, generation_time, delay
   regional_epinow(reported_cases = reported_cases,
                   generation_time = generation_time,
                   delays = delays,
-                  horizon = 14, burn_in = 7,
+                  horizon = 14, burn_in = 14,
                   samples = 2000, warmup = 500,
                   cores = no_cores, chains = ifelse(no_cores <= 2, 2, no_cores),
                   target_folder = target_dir,
