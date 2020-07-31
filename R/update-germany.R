3eb2a300fc99e261cf4644db3a880904d43cd27b
Sam Abbott <s.e.abbott12@gmail.com>
2020-07-31 10:12:34 +0100

    Update update-germany.R

--- R/update-germany.R
+++ R/update-germany.R
@@ -3,6 +3,7 @@ require(EpiNow2)
 require(covidregionaldata)
 require(data.table)
 require(future)
+require(lubridate)
 
 
 # Load utils --------------------------------------------------------------
@@ -25,7 +26,7 @@ cases <- clean_regional_data(cases[, region := bundesland])
 
 check_for_update(cases, last_run = here::here("last-update", "germany.rds"))
 
-# # Set up cores -----------------------------------------------------
+# Set up cores -----------------------------------------------------
 
 no_cores <- setup_future(length(unique(cases$region)))
 
