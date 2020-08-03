708730a35013fdf237acc06fcd4019bc5f780290
Sam Abbott <s.e.abbott12@gmail.com>
2020-08-03 18:37:35 +0100

    Update update-via-ssh.sh

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -4,7 +4,8 @@
 ## curl --fail https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/bin/update-via-ssh.sh > update-via-ssh.sh
 ## sudo bash update-via-ssh.sh path-to-key username@public-ip-of-server github-username github-pat
 ## Note this is not very secure and I assume there are better ways to do this
-ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4 << EOF
+ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4
+ssh -i $1 $2 << EOF
   sudo apt-get update -y
   sudo apt-get install -y docker.io
   echo "$GITHUB_PASSWORD" | docker login -username "$GITHUB_USERNAME" --password-stdin docker.pkg.github.com
