5f288f438d97919638587a363fd261a6e49c8e95
Sam Abbott <s.e.abbott12@gmail.com>
2020-08-03 17:58:11 +0100

    Update update-via-ssh.sh

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ## Run using:
-## curl --fail https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/bin/update-via-ssh.sh | sudo bash -s -- path-to-key username@public-ip-of-server
+## curl --fail https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/bin/update-via-ssh.sh --output update-via-ssh.sh
+## sudo bash update-via-ssh.sh path-to-key username@public-ip-of-server
 ssh -i $1 $2 << EOF
   sudo apt-get update -y
   sudo apt-get install -y docker.io
