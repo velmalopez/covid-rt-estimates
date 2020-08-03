7adc30c548e89671e558478ae8fc2b8e6128e769
Sam Abbott <s.e.abbott12@gmail.com>
2020-08-03 19:05:30 +0100

    Update update-via-ssh.sh

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -4,12 +4,10 @@
 ## curl --fail https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/bin/update-via-ssh.sh > update-via-ssh.sh
 ## sudo bash update-via-ssh.sh path-to-key username@public-ip-of-server github-username github-pat
 ## Note this is not very secure and I assume there are better ways to do this
-ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4 << EOF
-  sudo apt-get update -y
-  sudo apt-get install -y docker.io
-  echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com
-  git clone https://github.com/epiforecasts/covid-rt-estimates.git
-  cd covid-rt-estimates
-  sudo bash bin/update-docker.sh "build"
-  sudo bash bin/update-via-docker.sh
-EOF
+ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4 "sudo apt-get update -y && \
+  sudo apt-get install -y docker.io && \
+  echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com && \
+  git clone https://github.com/epiforecasts/covid-rt-estimates.git && \
+  cd covid-rt-estimates && \
+  sudo bash bin/update-docker.sh "build" && \
+  sudo bash bin/update-via-docker.sh"
