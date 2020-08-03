10d6346ad1500123ffc3f9ede0b55c6c19babb7f
Sam Abbott <s.e.abbott12@gmail.com>
2020-08-03 19:12:20 +0100

    Update update-via-ssh.sh

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -6,8 +6,8 @@
 ## Note this is not very secure and I assume there are better ways to do this
 ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4 "sudo apt-get update -y && \
   sudo apt-get install -y docker.io && \
-  # The docker login step here is failing - current work around is to ssh and docker login first
-  #echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com && \
+  \ # The docker login step here is failing - current work around is to ssh and docker login first
+  \ #echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com && \
   git clone https://github.com/epiforecasts/covid-rt-estimates.git && \
   cd covid-rt-estimates && \
   sudo bash bin/update-docker.sh "build" && \
