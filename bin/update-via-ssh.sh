bc8890e81856299047e6ef97a96aac93e35bd568
seabbs <s.e.abbott12@gmail.com>
2020-08-06 14:34:27 +0000

    updated docker login

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ssh -i $1 $2 GITHUB_USERNAME=$3 GITHUB_PASSWORD=$4 "sudo apt-get update -y && \
+ssh -i $1 $2  'export GITHUB_USERNAME='"'$3'"' && \ 
+  export GITHUB_PASSWORD='"'$4'"' && \
+  sudo apt-get update -y && \
   sudo apt-get install -y docker.io && \
-  echo "$GITHUB_PASSWORD" | sudo docker login docker.pkg.github.com --username $GITHUB_USERNAME --password-stdin  && \
+  echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com  && \
   git clone https://github.com/epiforecasts/covid-rt-estimates.git && \
   cd covid-rt-estimates && \
   sudo bash bin/update-docker.sh "build" && \
-  sudo bash bin/update-via-docker.sh"
+  sudo bash bin/update-via-docker.sh'
   
-  
-
