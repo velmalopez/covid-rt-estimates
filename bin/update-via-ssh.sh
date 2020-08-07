5fd1440b606b65fda8f070c9ae9560bcca3344d8
Sam Abbott <s.e.abbott12@gmail.com>
2020-08-07 13:31:49 +0100

    Update update-via-ssh.sh

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -5,8 +5,7 @@ ssh -i $1 $2  'export GITHUB_USERNAME='"'$3'"' && \
   sudo apt-get update -y && \
   sudo apt-get install -y docker.io && \
   echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com  && \
-  git clone https://github.com/epiforecasts/covid-rt-estimates.git && \
-  cd covid-rt-estimates && \
+  if cd covid-rt-estimates; then git pull; else git clone https://github.com/epiforecasts/covid-rt-estimates.git; cd covid-rt-estimates fi && \
   sudo bash bin/update-docker.sh "build" && \
   sudo bash bin/update-via-docker.sh'
   
