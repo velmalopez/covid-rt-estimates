97232de802d020e17b84c47d0d624949221524ee
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-08-13 16:50:17 +0100

    try abstracting the regional processing - save copying and pasting the logging into every copy!

---
+++ R/update-regional.R
@@ -0,0 +1,65 @@
+# Packages -----------------------------------------------------------------
+require(EpiNow2, quietly = TRUE)
+require(covidregionaldata, quietly = TRUE)
+require(data.table, quietly = TRUE)
+require(future, quietly = TRUE)
+require(lubridate, quietly = TRUE)
+
+# Load utils --------------------------------------------------------------
+
+source(here::here("R", "utils.R"))
+
+
+#' update.regional
+#'
+#' @description Processes regional data in an abstract fashion to reduce code duplication
+#' @param region_name String, name of region, used for filepaths
+#' @param region_identifier String, region identifier used by covidregionaldata to get the data
+#' @param case_modifier_function Function, passed the cases, should return the cases. Method of modifying the returned data for a specific region if needed
+#' @param generation_time optional overrides for the loaded rds file. If present won't be reloaded from disk.
+#' @param incubation_period optional overrides for the loaded rds file. If present won't be reloaded from disk.
+#' @param reporting_delay optional overrides for the loaded rds file. If present won't be reloaded from disk.
+update.regional <- function(region_name, region_identifier, case_modifier_function, generation_time = NULL, incubation_period = NULL, reporting_delay = NULL) {
+  futile.logger::flog.info("Processing regional dataset for %s", region_name)
+  # setting debug level to trace whilst still in beta. #ToDo: remove this line once production ready
+  futile.logger::flog.threshold(futile.logger::TRACE)
+
+  # Update delays -----------------------------------------------------------
+
+  if (!generation_time) {
+    generation_time <- readRDS(here::here("data", "generation_time.rds"))
+  }
+  if (!incubation_period) {
+    incubation_period <- readRDS(here::here("data", "incubation_period.rds"))
+  }
+  if (!reporting_delay) {
+    reporting_delay <- readRDS(here::here("data", "onset_to_admission_delay.rds"))
+  }
+
+  # Get cases  ---------------------------------------------------------------
+
+  futile.logger::flog.trace("Getting regional data")
+  cases <- data.table::setDT(covidregionaldata::get_regional_data(country = region_identifier))
+  futile.logger::flog.trace("Modifying regional data")
+  cases <- case_modifier_function(cases)
+  futile.logger::flog.trace("Cleaning regional data")
+  cases <- clean_regional_data(cases)
+
+  # Check to see if the data has been updated  ------------------------------
+
+  if (check_for_update(cases, last_run = here::here("last-update", paste0(region_name, ".rds")))) {
+
+    # Set up cores -----------------------------------------------------
+
+    no_cores <- setup_future(length(unique(cases$region)))
+
+    # Run Rt estimation -------------------------------------------------------
+
+    regional_epinow_with_settings(reported_cases = cases,
+                                  generation_time = generation_time,
+                                  delays = list(incubation_period, reporting_delay),
+                                  no_cores = no_cores,
+                                  target_dir = paste0("subnational/", region_name, "/cases/national"),
+                                  summary_dir = paste0("subnational/", region_name, "/cases/summary"))
+  }
+}
--- R/update-united-kingdom.R
+++ R/update-united-kingdom.R
@@ -1,45 +1,8 @@
-# Packages -----------------------------------------------------------------
-require(EpiNow2, quietly = TRUE)
-require(covidregionaldata, quietly = TRUE)
-require(data.table, quietly = TRUE)
-require(future, quietly = TRUE)
-require(here, quietly = TRUE)
-require(lubridate, quietly = TRUE)
+source(here::here("R", "update-regional.R"))
 
-# Load utils --------------------------------------------------------------
-
-source(here::here("R", "utils.R"))
-
-futile.logger::flog.info("Processing regional dataset for United Kingdom")
-
-# Update delays -----------------------------------------------------------
-
-generation_time <- readRDS(here::here("data", "generation_time.rds"))
-incubation_period <- readRDS(here::here("data", "incubation_period.rds"))
-reporting_delay <- readRDS(here::here("data", "onset_to_admission_delay.rds"))
-
-# Get cases  ---------------------------------------------------------------
-
-futile.logger::flog.trace("Getting regional data")
-cases <- data.table::setDT(covidregionaldata::get_regional_data(country = "UK"))
-futile.logger::flog.debug("%s regions returned", length(cases))
-
-cases <- clean_regional_data(cases)
-
-# Check to see if the data has been updated  ------------------------------
-
-if (check_for_update(cases, last_run = here::here("last-update", "united-kingdom.rds"))) {
-
-  # Set up cores -----------------------------------------------------
-
-  no_cores <- setup_future(length(unique(cases$region)))
-
-  # Run Rt estimation -------------------------------------------------------
-
-  regional_epinow_with_settings(reported_cases = cases,
-                                generation_time = generation_time,
-                                delays = list(incubation_period, reporting_delay),
-                                no_cores = no_cores,
-                                target_dir = "subnational/united-kingdom/cases/national",
-                                summary_dir = "subnational/united-kingdom/cases/summary")
-}
+update.regional("united-kingdom",
+                "UK",
+                function(cases) {
+                  return(cases)
+                }
+)
