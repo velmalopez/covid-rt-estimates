4d36f30f0225fcc4f4776598ff1a867a767f48af
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-08-13 17:02:27 +0100

    check if afghanistan works with a case modifier function

--- R/update-afghanistan.R
+++ R/update-afghanistan.R
@@ -1,40 +1,9 @@
-# Packages -----------------------------------------------------------------
-require(EpiNow2, quietly = TRUE)
-require(covidregionaldata, quietly = TRUE)
-require(data.table, quietly = TRUE)
-require(future, quietly = TRUE)
-require(lubridate, quietly = TRUE)
+source(here::here("R", "update-regional.R"))
 
-# Load utils --------------------------------------------------------------
-
-source(here::here("R", "utils.R"))
-
-# Update delays -----------------------------------------------------------
-
-generation_time <- readRDS(here::here("data", "generation_time.rds"))
-incubation_period <- readRDS(here::here("data", "incubation_period.rds"))
-reporting_delay <- readRDS(here::here("data", "onset_to_admission_delay.rds"))
-
-# Get cases  ---------------------------------------------------------------
-
-cases <- data.table::setDT(covidregionaldata::get_regional_data(country = "afghanistan"))
-
-cases <- cases[!is.na(iso_3166_2)]
-cases <- clean_regional_data(cases[, region := province])
-
-# Check to see if the data has been updated  ------------------------------
-
-check_for_update(cases, last_run = here::here("last-update", "afghanistan.rds"))
-
-# Set up cores -----------------------------------------------------
-
-no_cores <- setup_future(length(unique(cases$region)))
-
-# Run Rt estimation -------------------------------------------------------
-
-regional_epinow_with_settings(reported_cases = cases,
-                generation_time = generation_time,
-                delays = list(incubation_period, reporting_delay),
-                no_cores = no_cores,
-                target_dir = "subnational/afghanistan/cases/national",
-                summary_dir = "subnational/afghanistan/cases/summary")
+update.regional("afghanistan",
+                "afghanistan",
+                function(cases){
+                  cases <- cases[!is.na(iso_3166_2)]
+                  return(cases)
+                }
+)
