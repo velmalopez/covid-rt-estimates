e7beb71718d0074171acdb220989f3e9dbaf000f
zmaalick <zubair.maalick@metoffice.gov.uk>
2021-09-07 13:55:41 +0100

    delay

--- python/get-r-libraries.py
+++ python/get-r-libraries.py
@@ -81,14 +81,14 @@ def update_regional(datasets, region):
     #cases = clean_regional_data(cases, 3, 12)
 
     # Extracting parameters for EpiNow
-    generation_time = datasets[0]["generation_time"][0]
-    incubation_period = datasets[0]["incubation_period"][0]
-    reporting_delay = datasets[0]["reporting_delay"][0]
+    gtime = datasets[0]["generation_time"][0]
+    iperiod = datasets[0]["incubation_period"][0]
+    rdelay = datasets[0]["reporting_delay"][0]
     target_folder = datasets[0]["target_folder"][0]
 
 
     # Calling EpiNow
-    out = epinow2.regional_epinow(reported_cases = rdf, silent = True)
+    out = epinow2.regional_epinow(reported_cases = rdf, generation_time = gtime, delays = delay_opts(iperiod,rdelay) , silent = True)
 
 
 
